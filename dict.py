7d4590d5de7f4feb629bb2b8c3695768437e201e
jainsejal <sejaljain1194@gmail.com>
2020-05-19 14:23:08 +0200

    Python dictionary

---
+++ dict.py
@@ -0,0 +1,9 @@
+# Having 2 dicts using one dict do print stuff
+prices = {"banana": 4,"apple": 2,"orange": 1.5,"pear": 3}
+
+stock = {"banana": 6, "apple": 0, "orange": 32, "pear": 15}
+
+for food in prices:
+  print food
+  print "price: %s" % prices[food]
+  print "stock: %s" % stock[food]
