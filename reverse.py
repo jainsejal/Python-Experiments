205204e9e4d44a517e6486bc11849ebeda87ce58
jainsejal <sejaljain1194@gmail.com>
2020-05-27 20:14:13 +0200

    String reverse

---
+++ reverse.py
@@ -0,0 +1,10 @@
+#reversing the string. without using reserver function or [::-1]
+def reverse(text):
+    word = ""
+    l = len(text) - 1
+    while l >= 0:
+        word = word + text[l]
+        l -= 1
+    return word
+  
+print reverse("Hello World")
