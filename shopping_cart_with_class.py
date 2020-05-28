9d463a563c1c055110bf97509bc47ccaf3194555
jainsejal <sejaljain1194@gmail.com>
2020-05-28 19:55:18 +0200

    Classes with shopping cart

---
+++ shopping_cart_with_class.py
@@ -0,0 +1,26 @@
+class ShoppingCart(object):
+  """Creates shopping cart objects
+  for users of our fine website."""
+  
+  def __init__(self, customer_name):
+    self.customer_name = customer_name
+    self.items_in_cart = {}
+  def add_item(self, product, price):
+    """Add product to the cart."""
+    if not product in self.items_in_cart:
+      self.items_in_cart[product] = price
+      print product + " added."
+    else:
+      print product + " is already in the cart."
+
+  def remove_item(self, product):
+    """Remove product from the cart."""
+    if product in self.items_in_cart:
+      del self.items_in_cart[product]
+      print product + " removed."
+    else:
+      print product + " is not in the cart."
+
+my_cart = ShoppingCart("ruhi")
+#using the add_item method to add an item to your cart.
+my_cart.add_item("rice", 8)
